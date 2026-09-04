ef65cc9d627134f58bddd817e6192a3e6e96cf7a
2590413 <2590413@cegepoutaouais.qc.ca>
2026-09-04 13:55:39 -0400

    Modif classe (pas fini compte)

--- LaVieDeBob/Class1.cs
+++ LaVieDeBob/Class1.cs
@@ -3,47 +3,9 @@ using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
-public class Personne
-{
-    public string Nom { get;}
-    public string Prenom { get;}
-    public Emploi emploi { get; set; }
-    public Compte compte { get; set; }
 
-    public Personne()
-    {
-        Nom = "";
-        Prenom = "";
-        Emploi emploi = new Emploi();
-        Compte compte = new Compte();
-    }
 
-    public Personne(string Nom, string Prenom, Emploi emploi, Compte compte)
-    {
-        Nom = Nom;
-        Prenom = Prenom;
-        emploi = emploi;
-        compte = compte;
-    }
-}
 
-public class Emploi
-{
-    public string Titre { get; }
-    public decimal Salaire { get; }
-
-    public Emploi()
-    {
-        Titre = "";
-        Salaire = 0;
-    }
-
-    public Emploi(string Titre, decimal Salaire)
-    {
-        Titre = Titre;
-        Salaire = Salaire;
-    }
-}
 
 public class Compte
 {
---
+++ LaVieDeBob/Personne.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LaVieDeBob
+{
+    public class Personne
+    {
+        public string Nom { get; private set; }
+        public string Prenom { get; private set; }
+        public Emploi Emploi { get;}
+        public Compte Compte { get;}
+
+
+        public Personne(string prenom, string nom, string titreEmploi, decimal salaire)
+        {
+            Nom = Nom;
+            Prenom = Prenom;
+            Emploi = new (titreEmploi, salaire);
+            Compte = new Compte();
+        }
+
+        public Personne(string prenom) : this(prenom, "", "Sans emploi", 0)
+        { }
+
+        public decimal Travailler(int heures)
+        {
+            if (heures < 0)
+                throw new ArgumentOutOfRangeException("Le nombre d'heures travaillées doit être positif.")
+
+            decimal salaireGagne = heures * Emploi.salaire;
+            Compte.Deposer(salaireGagne);
+
+            return salaireGagne;
+        }
+
+        public decimal Nourrir(DateTime heureRepas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"Salut! Je m'appelle {Prenom} {Nom}. " +
+                   $"Je suis {Emploi.titre} et je gagne " +
+                   $"{Emploi.salaire:C}/h";
+        }
+    }
+}
